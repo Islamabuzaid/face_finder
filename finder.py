0b2631eff2d02abaf103175193a7937d58bd93e0
Islamabuzaid <Islamabuzaid19@gmail.com>
2026-05-06 17:42:42 -0400

    fix the label switching

--- finder.py
+++ finder.py
@@ -3,6 +3,7 @@ import PIL.ImageOps
 import numpy as np
 import face_recognition
 import cv2
+from collections import deque
 
 img = PIL.Image.open('img5.jpg').convert('RGB') # converts img to rgb format
 img = PIL.ImageOps.exif_transpose(img)  # fixes phone camera rotation
@@ -10,11 +11,14 @@ raed_img = np.array(img)
 
 encodings = face_recognition.face_encodings(raed_img)
 print(f"Found {len(encodings)} faces")
-brother_encoding = encodings[0] # grabs the first face found in th photo
+brother_encoding = encodings[0] # grabs the first face found in the photo
 
 frame_count = 0 
 face_encoding = []
 face_locations = []
+stable_match = False
+
+match_history = deque(maxlen=15) # keeps history of last 5 match results
 
 webcam = cv2.VideoCapture(0) # use 1 if you have external webcam
 while True:
@@ -28,8 +32,18 @@ while True:
 
 
     for(top, right, bottom, left), face_encoding in zip(face_locations, face_encoding):
-        match = face_recognition.compare_faces([brother_encoding], face_encoding)[0]
-        name = 'Freaky ash' if match else 'Unknown'
+        match = face_recognition.compare_faces([brother_encoding], face_encoding, tolerance = 0.5)[0]
+        match_history.append(match)
+
+        confidence = match_history.count(True) / len(match_history) # only label if most of the frames agree
+        
+        if confidence >= 0.50:
+            stable_match = True
+        elif confidence < 0.50:
+            stable_match = False
+
+
+        name = 'Freaky ash' if stable_match else 'Unknown'
 
         #draw box around face and labels it
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
@@ -40,5 +54,7 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
 webcam.release()
 cv2.destroyAllWindows()
